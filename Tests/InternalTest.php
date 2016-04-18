0fb0b20ed3278d6a90fd1466a46606419fb4d35c
pentagonal <pentagonalevelopment@gmail.com>
2016-04-18 23:59:46 +0700

    Change File Name to Run Test

---
+++ Tests/InternalTest.php
@@ -0,0 +1,66 @@
+<?php
+use Pentagonal\StaticHelper\InternalHelper;
+
+class TestInternal extends \PHPUnit_Framework_TestCase
+{
+    public function testStrtoLower()
+    {
+        $string = 'TheUppercase';
+        $arrayUppercase = array(
+            'TheUppercase',
+            'TheUppercaseLetter'
+        );
+        $this->assertNotEquals(
+            $string,
+            InternalHelper::strtoLower($string)
+        );
+        $this->assertNotEquals(
+            $arrayUppercase,
+            InternalHelper::strtoLower($arrayUppercase)
+        );
+    }
+
+    public function testStrtoUpper()
+    {
+        $string = 'TheLowercase';
+        $arrayLowerCase = array(
+            'TheLowercase',
+            'TheLowercaseLetter'
+        );
+        $this->assertNotEquals(
+            $string,
+            InternalHelper::strtoUpper($string)
+        );
+        $this->assertNotEquals(
+            $arrayLowerCase,
+            InternalHelper::strtoUpper($arrayLowerCase)
+        );
+    }
+
+    public function testStrSplit()
+    {
+        $string = 'TestToSplitasArray';
+        $this->assertArrayHasKey(
+            1,
+            InternalHelper::strSplit($string, 2)
+        );
+    }
+
+    public function testBase64Encode()
+    {
+        $string = 'plain';
+        $this->assertEquals(
+            base64_encode($string),
+            InternalHelper::base64Encode($string)
+        );
+    }
+
+    public function testBase64ecode()
+    {
+        $string = base64_encode('plain');
+        $this->assertEquals(
+            base64_decode($string),
+            InternalHelper::base64Decode($string)
+        );
+    }
+}
